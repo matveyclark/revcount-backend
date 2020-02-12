f6ef86f7f639300a8e49f3eb4b5311900c2b68d5
Matthew <matthew@stateofmind.agency>
2020-02-12 17:10:50 +0000

    API for clients: register, show, login and validate - working.

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,3 +1,36 @@
 class ClientController < ApplicationController
 
+    def create
+        begin
+            client = Client.create(client_params)
+            render json: { status: "success", data: client, token: issue_token({ id: client.id }) }, status: 201
+        rescue
+            render json: { error: "Please enter valid inputs to regsiter." }, status: 401
+        end
+    end
+
+    def show
+        client = Client.find_by(id: params[:id])
+        if client
+            render json: { status: "success", data: client }
+        else
+            render json: { error: "There is no client with that ID." }
+        end
+    end
+
+    def login
+        client = Client.find_by(email: client_params[:email])
+        if client && client.authenticate(client_params[:password])
+            render json: { status: "success", user: client.email, token: issue_token({ id: client.id })}, status: 200
+        else
+            render json: { error: "Incorrect password or username."}, status: 401
+        end
+    end
+
+    private
+
+    def client_params
+        params.require(:client).permit(:first_name, :last_name, :email, :password)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Project Manager routes
-  # Route to register a new project manager
   post '/register-pm' => 'project_manager#create'
   get '/project-manager/:id' => 'project_manager#show'
   post '/login-pm' => 'project_manager#login'
   get '/validate-pm' => 'project_manager#validate'
+
+  # Client routes
+  post '/register' => 'client#create'
+  get '/client/:id' => 'client#show'
+  post '/login' => 'client#login'
 end
